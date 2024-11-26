774d8b1f03bd7147fe472450d365b93a2b389156
miguelestradam36 <miguelestradam36@gmail.com>
2024-11-25 18:59:44 -0600

    Added styles/content to the footer

--- index.php
+++ index.php
@@ -234,10 +234,10 @@
     <div class="container footer-top">
       <div class="row gy-4">
         <div class="col-lg-5 col-md-12 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">Logis</span>
+          <a href="index.php" class="logo d-flex align-items-center">
+            <span class="sitename">Calculadora de salario neto</span>
           </a>
-          <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+          <p>Sitio web que permite el cálculo del salario neto en base al salario bruto mensual, junto a otras estadísticas</p>
           <div class="social-links d-flex mt-4">
             <a href=""><i class="bi bi-twitter-x"></i></a>
             <a href=""><i class="bi bi-facebook"></i></a>
@@ -246,35 +246,21 @@
           </div>
         </div>
 
-        <div class="col-lg-2 col-6 footer-links">
-          <h4>Useful Links</h4>
+        <div class="col-lg-3 col-6 footer-links">
+          <h4>Otros proyectos</h4>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
+            <li><a href="#">Proyecto numero 1</a></li>
+            <li><a href="#">Proyecto numero 1</a></li>
+            <li><a href="#">Proyecto numero 1</a></li>
           </ul>
         </div>
 
-        <div class="col-lg-2 col-6 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-          <h4>Contact Us</h4>
-          <p>A108 Adam Street</p>
-          <p>New York, NY 535022</p>
-          <p>United States</p>
-          <p class="mt-4"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-          <p><strong>Email:</strong> <span>info@example.com</span></p>
+        <div class="col-lg-4 col-md-12 footer-contact text-center text-md-start">
+          <h4>Contáctenos</h4>
+          <p>Moravia, San Jose</p>
+          <p>Costa Rica</p>
+          <p class="mt-4"><strong>Phone:</strong> <span>N/A</span></p>
+          <p><strong>Email:</strong> <span>N/A</span></p>
         </div>
 
       </div>
